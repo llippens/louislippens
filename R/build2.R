793bbb1d213769f6255d2e9e855e8e1caaf86699
lglip <lippens.louisl@gmail.com>
2021-07-09 01:22:14 +0200

    Merge fixes.

--- R/build2.R
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-# An optional custom script to run after Hugo builds your site.
-# You can delete it if you do not need it.
-=======
-# An optional custom script to run after Hugo builds your site.
-# You can delete it if you do not need it.
->>>>>>> 5df9bc554174fd97f8de5bd48a25b35e12803f2e
